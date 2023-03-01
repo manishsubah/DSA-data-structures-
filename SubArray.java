5867fcdc3e36443fa4e27913e254e459e51fc031
Manish Ranjan <manishranjan.soft@gmail.com>
2023-03-01 17:13:15 +0530

    Initial Commit

---
+++ SubArray.java
@@ -0,0 +1,25 @@
+class SubArray {
+    public static void sArray(int number[]) {
+        int sum = 0;
+        for(int i=0; i<number.length; i++) {
+            int start = i;
+            for(int j=i; j<number.length; j++) {
+                int end = j;
+                for(int k=start; k<=end; k++) {
+                    //Print Sub-Arrays
+                    System.out.print(number[k]+" ");
+                    // Sum of Sub-Arrays
+                    sum = sum + number[k];
+                }
+                // Print Sum of Sub-Arrays
+                System.out.print("Sum= "+sum);
+                sum = 0;
+                System.out.println();
+            }
+        }
+    }
+    public static void main(String args[]) {
+        int number[] = {2,7,11,15};//{2, 5, 7, 8, 6, 9};
+        sArray(number);
+    }
+}
